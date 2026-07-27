d907116c7871fcffc0fc20b06c12b86b3e21d4ef
Lakshay <lakshay.cherukuri@gmail.com>
2026-07-27 17:53:16 +0800

    Create maximum.cpp

---
+++ maximum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main(){
+long long a;
+long long b;
+cin >> a;
+cin >> b;
+if ( a > b ){
+	cout << a;
+}
+else{
+	cout << b;
+}
+	return 0;
+}
