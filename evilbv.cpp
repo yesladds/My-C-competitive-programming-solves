9423d00a5209d2a6aaf7731856a7241c55e5aa72
Lakshay <lakshay.cherukuri@gmail.com>
2026-07-27 17:45:25 +0800

    Create evilbv.cpp

---
+++ evilbv.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue> 
+using namespace std;
+#define int long long
+
+signed main() {
+string s;
+string a;
+cin >> s;
+if (s == "bz")
+{cout << "CPIC";}
+else 
+{cout << "ICPC";}
+	
+    
+	
+		
+}
