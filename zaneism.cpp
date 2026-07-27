7f0b60dd3b57fa34487d571bb866e3d1e1aa409f
Lakshay <lakshay.cherukuri@gmail.com>
2026-07-27 17:27:22 +0800

    Create zaneism.cpp

---
+++ zaneism.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main(){
+cout << "I believe in Zane, the Creator of CPIC Land. Bestow upon us thy courage and wisdom for a Gold Medal in NOI 2027.";
+	return 0;
+}
