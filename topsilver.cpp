8852b266b56c9c587b16775d5548b1520f1738fa
Lakshay <lakshay.cherukuri@gmail.com>
2026-07-27 17:42:39 +0800

    Create topsilver.cpp

---
+++ topsilver.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main(){
+int X;
+int Y;
+cin >> X >> Y;
+if ( X == Y + 1 ){
+	cout << "top silver";}
+else if ( Y < X ){
+	cout << "womp womp";}
+else{
+	cout << "lucky";}				
+	return 0;
+	
+}
