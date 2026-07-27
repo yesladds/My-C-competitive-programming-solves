72fc7e049f54394be7847e0f5a9eb574f8067fa8
Lakshay <lakshay.cherukuri@gmail.com>
2026-07-27 17:52:18 +0800

    Create exam.cpp

---
+++ exam.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main(){
+long long x;
+cin >> x;
+if ( x == 1 ){
+cout << "Hello World";
+}
+else if ( x == 2 ){
+long long A;
+long long B;
+cin >> A >> B;
+cout << A * B;
+}
+return 0;
+}
